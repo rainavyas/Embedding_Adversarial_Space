c3a82a4ce717782a5f6768b20a6b038e2b0e6206
Vyas Raina <sayvaniar@gmail.com>
2021-04-12 20:37:04 +0100

    batching

--- perturb_bert_layern.py
+++ perturb_bert_layern.py
@@ -17,7 +17,7 @@ import argparse
 from models import BertSequenceClassifier
 from pca_tools import get_covariance_matrix, get_e_v
 import matplotlib.pyplot as plt
-from tools import fooling_rate
+from tools import fooling_rate, AverageMeter
 from layer_handler import Bert_Handler
 
 
@@ -27,28 +27,32 @@ def make_attack(vec, epsilon):
     attackB = -1*attackA
     return attackA, attackB
 
-def get_perturbation_impact(handler, v, input_ids, mask, labels, model, epsilon, stepsize=1, token_pos=0):
+def get_perturbation_impact(handler, v, input_ids, mask, labels, model, epsilon, stepsize=1, token_pos=0, batch_size=32):
     ranks  = []
     fools = []
 
+    # batch the data
+    ds = TensorDataset(input_ids, mask, labels)
+    dl = DataLoader(ds, batch_size=batch_size)
     model.eval()
-    # Get original logits
-    original_logits = model(input_ids, mask)
 
     for i in range(0, v.size(0), stepsize):
         print("On rank", i)
         ranks.append(i)
         curr_v = v[i]
-        with torch.no_grad():
-            attack, _ = make_attack(curr_v, epsilon)
-            layer_embeddings = handler.get_layern_outputs(input_ids, mask)
-            layer_embeddings[:,token_pos,:] = layer_embeddings[:,token_pos,:] + attack
+        fool = AverageMeter()
+        with torch.no_data():
+            for id, m, lab in dl:
+                original_logits = model(id, m)
+                attack, _ = make_attack(curr_v, epsilon)
+                layer_embeddings = handler.get_layern_outputs(id, m)
+                layer_embeddings[:,token_pos,:] = layer_embeddings[:,token_pos,:] + attack
 
-            # Pass through rest of model
-            attacked_logits = handler.pass_through_rest(layer_embeddings, mask)
-            fool = fooling_rate(original_logits, attacked_logits)
-            fools.append(fool.item())
-
+                # Pass through rest of model
+                attacked_logits = handler.pass_through_rest(layer_embeddings, m)
+                curr_fool = fooling_rate(original_logits, attacked_logits)
+                fool.update(curr_fool.item(), id.size(0))
+        fools.append(fool.avg)
     return ranks, fools
 
 def plot_data_vs_rank(ranks, data, yname, filename):
@@ -70,6 +74,7 @@ if __name__ == '__main__':
     commandLineParser.add_argument('--layer_num', type=int, default=1, help="BERT layer to perturb")
     commandLineParser.add_argument('--stepsize', type=int, default=1, help="ranks step size for plot")
     commandLineParser.add_argument('--num_points', type=int, default=25000, help="number of data points to use")
+    commandLineParser.add_argument('--batch_size', type=int, default=64, help="batch size")
 
 
     args = commandLineParser.parse_args()
@@ -80,6 +85,7 @@ if __name__ == '__main__':
     layer_num = args.layer_num
     stepsize = args.stepsize
     num_points = args.num_points
+    batch_size = args.batch_size
 
     # Save the command run
     if not os.path.isdir('CMDs'):
@@ -111,7 +117,7 @@ if __name__ == '__main__':
     labels = labels[:num_points]
 
     # Perturb in each eigenvector direction vs rank
-    ranks, fools = get_perturbation_impact(handler, v, input_ids, mask, labels, model, epsilon, stepsize=stepsize, token_pos=token_pos)
+    ranks, fools = get_perturbation_impact(handler, v, input_ids, mask, labels, model, epsilon, stepsize=stepsize, token_pos=token_pos, batch_size=batch_size)
 
     # Plot the data
     filename = 'fools_eigenvector_perturb_layer'+str(layer_num)+'_tokenpos'+str(token_pos)+'_epsilon'+str(epsilon)+'.png'
